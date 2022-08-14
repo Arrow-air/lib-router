e33d775bbeb6880c88791ec4d4d526316254aef1
Xipu Li <lixipu98@gmail.com>
2022-08-13 20:29:42 -0700

    Rename the library to lib_router

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,9 +1,9 @@
 //! Hello world example for Rust.
 //! This is a crate description, needed or else missing_docs warning will occur.
 
-use lib_template_rust;
+use lib_router;
 
 #[test]
 fn it_add_one() {
-    assert_eq!(2, lib_template_rust::add_one(1));
+    assert_eq!(2, lib_router::add_one(1));
 }
