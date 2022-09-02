2f7048f9bf5859929c1e29bac3eb2bb05600ccbb
Xipu Li <lixipu98@gmail.com>
2022-09-02 08:47:32 -0700

    Conform to imperative code style

--- src/types/status.rs
+++ src/types/status.rs
@@ -1,9 +1,11 @@
 //! Definition for the [`Status`] type, implemented by an enum.
 
-/// Represents the operating status of a [`super::node::Node`].
-#[derive(Debug, PartialEq, Hash, Eq)]
+/// Represent the operating status of a [`super::node::Node`].
+#[derive(Debug, PartialEq, Hash, Eq, Copy, Clone)]
 #[allow(dead_code)]
 pub enum Status {
+    /// Indicate that the node is currently operating.
     Ok,
+    /// Indicate that the node is currently down.
     Closed,
 }
