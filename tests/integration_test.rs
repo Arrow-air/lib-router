0666a9adb8253f308f9cdf3849e1f8c375ff7842
Xipu Li <lixipu98@gmail.com>
2022-09-02 08:41:17 -0700

    feat: implement generator functions
    Signed-off-by: owlot <95369289+owlot@users.noreply.github.com>

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,9 +1,2 @@
 //! Hello world example for Rust.
 //! This is a crate description, needed or else missing_docs warning will occur.
-
-use lib_router;
-
-#[test]
-fn it_add_one() {
-    assert_eq!(2, lib_router::add_one(1));
-}
